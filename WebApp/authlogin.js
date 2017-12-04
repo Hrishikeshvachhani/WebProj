cd3ed011735d91834e6fd34269d359450ae71e18
prahar4954 <34216391+prahar4954@users.noreply.github.com>
2017-12-04 20:17:08 +0530

    Add files via upload
    Please Check the alert massage in authlogin.js
    Run It.
    Why it not displays any output?

--- WebApp/authlogin.js
+++ WebApp/authlogin.js
@@ -1,11 +1,24 @@
-function toggleSignIn() {
-      if (firebase.auth().currentUser) {
-        // [START signout]
-        firebase.auth().signOut();
-        // [END signout]
-      } else {
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
+(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDomzPTwcQtQLzn4xB4Nh3ydFGysydD_8o",
+    authDomain: "webapp-34ae0.firebaseapp.com",
+    databaseURL: "https://webapp-34ae0.firebaseio.com",
+    projectId: "webapp-34ae0",
+    storageBucket: "",
+    messagingSenderId: "518074473208"
+  };
+  firebase.initializeApp(config);
+
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  var login = document.getElementById('login');
+
+login.addEventListener('click' , e=> {
+        var email = email.value;
+        var password = password.value;
+        var auth = firebase.auth();
+
         if (email.length < 4) {
           alert('Please enter an email address.');
           return;
@@ -14,9 +27,10 @@ function toggleSignIn() {
           alert('Please enter a password.');
           return;
         }
+        
         // Sign in with email and pass.
         // [START authwithemail]
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        /*firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
           // Handle Errors here.
           var errorCode = error.code;
           var errorMessage = error.message;
@@ -27,12 +41,10 @@ function toggleSignIn() {
             alert(errorMessage);
           }
           console.log(error);
-          document.getElementById('quickstart-sign-in').disabled = false;
+          
           // [END_EXCLUDE]
-        });
+        });*/
         // [END authwithemail]
-      }
-      document.getElementById('quickstart-sign-in').disabled = true;
-    }
+      });
     
-
+});
