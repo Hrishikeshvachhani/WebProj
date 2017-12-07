84227fbf01532bd410b143cc116db41618791803
prahar4954 <34216391+prahar4954@users.noreply.github.com>
2017-12-07 13:43:11 +0530

    Add files via upload

---
+++ New_Template/style.js
@@ -0,0 +1,12 @@
+var b=document.getElementsByTagName("button")[0];
+b.addEventListener("mouseover",function(){
+	b.style.background="blue";
+	b.style.color="white";
+
+},false);
+b.addEventListener("mouseout",function(){
+	b.style.background="#cde5ef";
+	b.style.color="#527881";
+
+},false);
+
